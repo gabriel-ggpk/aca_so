7e6da4f47152819ec7e234370467e020bef1d7be
gabriel-ggpk <gabriel.cavalcanti.28@gmail.com>
2022-12-22 13:35:15 -0300

    Adicionada estilização para o componente Input
    Adicionada estilização para o componente Input alem de propriedades para alterar o tamanho do componente.

--- src/components/elements/input/index.tsx
+++ src/components/elements/input/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 // usando export para conjtrnar um bug do eslint
 export interface Props {
   type: string;
-  placeholder?: string | 'Enter your email';
+  placeholder?: string;
+  width: string;
 }
+const StyledInput = styled.input<Props>`
+  border: none;
+  background-color: #1E1F2F;
+  height: 50px;
+  width: ${(props) => props.width || '200px'};
+  border-radius: 5px;
+  color: white;
+  padding: 16px 16px 10px 16px;
+  font-size: 16px;
+  box-sizing: border-box;
+  `;
 
-export default function Input({ type, placeholder }:Props): JSX.Element {
+export default function Input({
+  type,
+  placeholder,
+  width,
+}: Props): JSX.Element {
   const [value, setValue] = React.useState('');
 
   const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -14,8 +31,12 @@ export default function Input({ type, placeholder }:Props): JSX.Element {
   };
 
   return (
-    <div className="wrapper">
-      <input className="input" type={type} value={value} placeholder={placeholder} onChange={inputHandler} />
-    </div>
+    <StyledInput
+      type={type}
+      width={width}
+      value={value}
+      placeholder={placeholder}
+      onChange={inputHandler}
+    />
   );
 }
